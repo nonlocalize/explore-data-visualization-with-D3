6cb2467c7dd01d771ae8154edfb9dad961fbf3c4
Rob Brennan <rob@therobbrennan.com>
2019-05-23 15:53:29 -0700

    Fixed issue where removing event listener may result in a box in a "stuck" state

--- examples/05-interactions/1-events/js/events.js
+++ examples/05-interactions/1-events/js/events.js
@@ -1,21 +1,18 @@
 async function createEvent() {
-  const rectColors = [
-    "yellowgreen",
-    "cornflowerblue",
-    "seagreen",
-    "slateblue",
-  ]
+  const rectColors = ['yellowgreen', 'cornflowerblue', 'seagreen', 'slateblue']
 
   // create and bind data to our rects
-  const rects = d3.select("#svg")
-    .selectAll(".rect")
+  const rects = d3
+    .select('#svg')
+    .selectAll('.rect')
     .data(rectColors)
     // For all new data points, append a rect
-    .enter().append("rect")
-      .attr("height", 100)
-      .attr("width", 100)
-      .attr("x", (d,i) => i * 110)
-      .attr("fill", "lightgrey")
+    .enter()
+    .append('rect')
+    .attr('height', 100)
+    .attr('width', 100)
+    .attr('x', (d, i) => i * 110)
+    .attr('fill', 'lightgrey')
 
   // // Let's inspect a D3 event listener
   // rects.on("mouseenter", function(datum, index, nodes) {
@@ -35,17 +32,21 @@ async function createEvent() {
   // IMPORTANT: Do NOT use fat arrow functions here. We need to access the targeted element with this and not the lexical scope fat arrow functions give us.
   rects
     // Set the fill color of the box when the mouse enters
-    .on("mouseenter", function(datum, index, nodes) { d3.select(this).style("fill", datum) })
+    .on('mouseenter', function(datum, index, nodes) {
+      d3.select(this).style('fill', datum)
+    })
     // Fall back to grey when the mouse leaves
-    .on("mouseout", function() { d3.select(this).style("fill", "lightgrey") })
+    .on('mouseout', function() {
+      d3.select(this).style('fill', 'lightgrey')
+    })
 
   // Remove a D3 event listener by passing null as the triggered function
   setTimeout(() => {
-    console.log(`Removing D3 event listeners`)
-    rects
-      .on("mouseenter", null)
-      .on("mouseout", null)
-  }, 3000)
+    // This removes our listeners, but notice how we can have a box "stuck" in a mouseenter state...
+    // rects.on('mouseenter', null).on('mouseout', null)
 
+    // For this example, we can dispatch a mouseout event to ensure our boxes are not "stuck" before passing null
+    rects.dispatch("mouseout").on('mouseenter', null).on('mouseout', null)
+  }, 3000)
 }
 createEvent()
