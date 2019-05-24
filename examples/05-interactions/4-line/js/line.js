fd4c454c5be41102e1aff71d55e3d701f62baf7f
Rob Brennan <rob@therobbrennan.com>
2019-05-24 11:38:50 -0700

    Added initial tooltip to demo 4

--- examples/05-interactions/4-line/js/line.js
+++ examples/05-interactions/4-line/js/line.js
@@ -99,5 +99,52 @@ async function drawLineChart() {
 
   // 7. Set up interactions
 
+  // For a timeline, we want to display a tooltip whenever a hover occurs anywhere on the chart, so we need to create an element that spans our entire bounds
+  const listeningRect = bounds.append("rect")
+    // We don't need to define x or y attributes because they both default to 0 for rect elements
+    .attr("class", "listening-rect")
+    .attr("width", dimensions.boundedWidth)
+    .attr("height", dimensions.boundedHeight)
+    .on("mousemove", onMouseMove)
+    .on("mouseleave", onMouseLeave)
+
+  const tooltip = d3.select("#tooltip")
+  function onMouseMove() {
+    // How will we know the location on our line that we are hovering over?
+    const mousePosition = d3.mouse(this)  // this refers to listeningRect
+    // We can use the .invert() method of xScale() to convert our units backward - from the range to the domain, instead of from the domain to the range (default)
+    const hoveredDate = xScale.invert(mousePosition[0])
+
+    // Find the distance between the hovered point and a datapoint - use the absolute value since we don't care if the point is before or after the hovered date
+    const getDistanceFromHoveredDate = d => Math.abs(xAccessor(d) - hoveredDate)
+
+    // Use .scan() to get the index of the closest data point to our hovered date
+    const closestIndex = d3.scan(dataset, (a, b) => (getDistanceFromHoveredDate(a) - getDistanceFromHoveredDate(b)))
+    const closestDataPoint = dataset[closestIndex]
+    const closestXValue = xAccessor(closestDataPoint)
+    const closestYValue = yAccessor(closestDataPoint)
+
+    // Use the closestXValue to set the date in our tooltip
+    const formatDate = d3.timeFormat("%A, %B %d, %Y")
+    tooltip.select("#date").text(formatDate(closestXValue))
+
+    // Use the closestYValue to set the temperature in our tooltip
+    const formatTemperature = d => `${d3.format(".1f")(d)}°F`
+    tooltip.select("#temperature").text(formatTemperature(closestYValue))
+
+    // Grab the x,y position of our closest point
+    const x = xScale(closestXValue) + dimensions.margin.left
+    const y = yScale(closestYValue) + dimensions.margin.top
+
+    // Shift our tooltip
+    tooltip.style("transform", `translate(calc(-50% + ${x}px), calc(-100% + ${y}px))`)
+
+    // Display our tooltip
+    tooltip.style("opacity", 1)
+  }
+  function onMouseLeave() {
+    tooltip.style("opacity", 0) // Hide our tooltip
+  }
+
 }
 drawLineChart()
