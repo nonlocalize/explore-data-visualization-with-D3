ecb4f44da87cf24bf65ff964d39e5278dd5a4e0f
Johnny5 <johnny5@generalui.com>
2019-05-31 14:39:58 -0700

    Getting rid of Angular. Who cares?

--- fullstack-d3-advanced-code/14-using-d3-with-angular/src/app/chart/circles/circles.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core'
-
-@Component({
-  selector: '[appCircles]',
-  template: ``,
-  styleUrls: ['./circles.component.css']
-})
-export class CirclesComponent {
-}
--- fullstack-d3-advanced-code/14-using-d3-with-angular/src/app/chart/utils.ts
+++
@@ -1,9 +0,0 @@
-export const useAccessor = (accessor, d, i) => (
-  typeof accessor == "function" ? accessor(d, i) : accessor
-)
-
-let lastId = 0
-export const getUniqueId = (prefix="") => {
-  lastId++
-  return [prefix, lastId].join("-")
-}
